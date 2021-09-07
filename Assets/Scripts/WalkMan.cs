f8ee6b647c6c7b75210ffdc1b24f432e1924ca34
Michael Feldman <michael.feldman09@gmail.com>
2021-09-07 17:53:59 -0400

    Added unity project, walk code, collision, and physics

---
+++ Assets/Scripts/WalkMan.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkMan : MonoBehaviour
+{
+	Vector3 _flatForward;
+	Vector3 _flatRight;
+	Vector3 _groundPoint;
+	Vector3 _prevPos;
+	Vector3 _movement;
+
+	public LayerMask _walkable;
+
+	float _playerHeight=1.5f;
+	float _maxDropHeight=10f;
+	float _stepHeight=0.5f;
+
+	//speed
+	float _walkSpeed;
+	float _defaultWalkSpeed=5f;
+	float _yLerp=5f;
+
+	//collision
+	Collider[] _colliders;
+	int _maxColliders=10;
+	float _wallHitRadius=0.5f;
+
+	//fall physics
+	float _grav=10;
+	float _fallVel=0;
+	float _fallFactor=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		SnapStartingHeight();
+		_walkSpeed=_defaultWalkSpeed;
+		_colliders = new Collider[_maxColliders];
+    }
+	
+	void SnapStartingHeight(){
+		RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 100f, _walkable))
+        {
+			_groundPoint=hit.point;
+			transform.position=_groundPoint+Vector3.up*_playerHeight;
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		_flatForward=transform.forward;
+		_flatForward.y=0;
+		_flatForward.Normalize();
+		_flatRight = Vector3.Cross(Vector3.up,_flatForward);
+
+		//try movement
+		_movement = Vector3.zero;
+		_movement+=_flatForward*Input.GetAxis("Vertical")*Time.deltaTime*_walkSpeed;
+		_movement+=_flatRight*Input.GetAxis("Horizontal")*Time.deltaTime*_walkSpeed;
+		transform.position+=_movement;
+
+		//check for wall
+		int numColliders= Physics.OverlapSphereNonAlloc(transform.position, 
+				_wallHitRadius, _colliders);
+		Vector3 closePoint=Vector3.zero;
+
+		int attempts=0;
+		while(numColliders>0&&attempts<5){
+			for(int i=0; i<numColliders; i++){
+				closePoint=_colliders[i].ClosestPoint(transform.position);
+				//check if transform is still within collider
+				if((closePoint-_colliders[i].transform.position).sqrMagnitude <
+						(transform.position-_colliders[i].transform.position).sqrMagnitude)
+				{
+					//offset player from wall
+					transform.position=closePoint+
+						(transform.position-closePoint).normalized*_wallHitRadius;
+				}
+			}
+
+			numColliders= Physics.OverlapSphereNonAlloc(transform.position, 
+					_wallHitRadius, _colliders);
+			attempts++;
+		}
+
+		//check for ground
+		RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 100f, _walkable))
+        {
+			//stepping up
+			if(hit.distance<_playerHeight && hit.distance>_playerHeight-_stepHeight){
+				_groundPoint=hit.point;
+			}
+			//stepping down
+			else if(hit.distance>_playerHeight){
+				//step down
+				if(hit.distance<_playerHeight+_stepHeight){
+					_groundPoint=hit.point;
+				}
+				//drop down
+				else if(hit.distance<_playerHeight+_maxDropHeight){
+					_groundPoint=hit.point;
+					if(_fallVel==0)
+					{
+						_fallVel=0.1f;
+						_fallFactor=((transform.position.y-_playerHeight)-hit.point.y)/
+							_maxDropHeight;
+					}
+					//temp - until we code in drop
+					//transform.position=_prevPos;
+				}
+				//cliff
+				else{
+					//no jump
+					transform.position=_prevPos;
+				}
+			}
+			//flat walking
+			else{
+				_groundPoint=hit.point;
+				//transform.position=_groundPoint+Vector3.up*_playerHeight;
+			}
+        }
+
+		//animate y coord
+		Vector3 p = transform.position;
+		//falling
+		if(_fallVel>0){
+			_fallVel+=_grav*Time.deltaTime;
+			p.y-=_fallVel*Time.deltaTime;
+			//done falling
+			if(p.y<_groundPoint.y+_playerHeight-_stepHeight*_fallFactor)
+				_fallVel=0;
+		}
+		//normal walking or step up/down
+		else
+			p.y=Mathf.Lerp(p.y,_groundPoint.y+_playerHeight,_yLerp*Time.deltaTime);
+		transform.position=p;
+
+		_prevPos=transform.position;
+    }
+
+	void OnDrawGizmos(){
+		Gizmos.color=Color.blue;
+		Gizmos.DrawRay(transform.position,_flatForward);
+		Gizmos.color=Color.red;
+		Gizmos.DrawRay(transform.position,_flatRight);
+		Gizmos.color=Color.yellow;
+		Gizmos.DrawSphere(_groundPoint,0.15f);
+		Gizmos.color=Color.green;
+		Gizmos.DrawWireSphere(transform.position,0.5f);
+	}
+}
---
+++ Assets/Shaders/Checker.shader
@@ -0,0 +1,55 @@
+﻿Shader "Custom/Checker"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _ColorB ("Color B", Color) = (1,1,1,1)
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+
+        struct Input
+        {
+			fixed3 worldPos;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _Color;
+        fixed4 _ColorB;
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+			fixed xMod = step(0,abs(round(IN.worldPos.x))%2-1);
+			fixed zMod = step(0,abs(round(IN.worldPos.z))%2-1);
+			fixed checker = abs((xMod+zMod)-1);
+			fixed4 c = lerp(_Color,_ColorB,checker);
+            o.Albedo = c.rgb;
+            // Metallic and smoothness come from slider variables
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
